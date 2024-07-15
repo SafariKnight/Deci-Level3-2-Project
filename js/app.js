dc4b15fbec7494bac362ff4ae4a3bd5181db384e
Kareem Hisham <safariknight2@gmail.com>
2024-07-15 19:26:27 +0300

    feat(js): generated nav items

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
-/** TODO: Build the navigation bar using JavaScript **/
+/** Navigation Bar Code **/
 
-/** TODO: Add smooth scrolling **/
+/* Generating the navigation bar */
+// Finding the sections
+const sections = document.querySelectorAll("section");
 
-/** TODO: Add an active state **/
+// Generating the navigation bar items
+const navBarList = document.querySelector("#navbar__list");
+sections.forEach((section) => {
+  // Create the elements
+  const navBarItem = document.createElement("li");
+  const navBarLink = document.createElement("a");
+
+  // Modify the properties of the anchor
+  navBarLink.href = `#${section.id}`;
+
+  // Add the elements to the HTML
+  navBarItem.append(navBarLink);
+  navBarList.append(navBarItem);
+
+
+});
+
+
+
 
 /** TODO: Add a comment form **/
