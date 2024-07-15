10311820680833ba75157f5b7a4dee91c2c1a780
Kareem Hisham <safariknight2@gmail.com>
2024-07-15 19:26:27 +0300

    feat(js): added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -32,6 +32,19 @@ sections.forEach((section) => {
   })
 });
 
+/* Smooth Scrolling */
+
+// Event Delegation
+navBarList.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevents the default scrolling
+  const anchor = e.target; // Gets which achor was actually clicked on
+
+  // Scroll to section
+  document
+    // If anchor.href was used it would return the entire address
+    .querySelector(anchor.getAttribute("href")) // Get the section the anchor refers to
+    .scrollIntoView({ behavior: "smooth" }); // Scroll untili the section is visible (`{ behaviour: "smooth"}` ensures it scrolls to it smoothly)
+});
 
 
 
